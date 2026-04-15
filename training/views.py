9027e07dd2afc83273075a6f9054d2b4696ee903
Danny Muñoz <munozd17@gmail.com>
2026-04-14 21:42:36 -0400

    add ActivityListView

--- training/views.py
+++ training/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Block, Cycle, Activity, Segment
 
-# Create your views here.
+def ActivityListView(request):
+    model = Activity
+    template_name = "activities.html"
+    context_object_name = "activities"
+    ordering = ["-timestamp"]
