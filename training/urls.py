e4658898cfda3a7c9e5cd2b7af45588f1cb8c5ef
Danny Muñoz <munozd17@gmail.com>
2026-04-17 17:49:12 -0400

    add list view for blocks

--- training/urls.py
+++ training/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ActivityListView, TemplateView, IndexView
+from .views import ActivityListView, TemplateView, IndexView, BlockListView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('activities/', ActivityListView.as_view(), name='activity-list'),
+    path('blocks/', BlockListView.as_view(), name='block-list'),
 ]
--- training/views.py
+++ training/views.py
@@ -3,11 +3,18 @@ from django.views.generic import ListView, TemplateView
 from .models import Block, Cycle, Activity, Segment
 
 class IndexView(TemplateView):
-    template_name = "index.html"
+    template_name = 'index.html'
+
+
+class BlockListView(ListView):
+    model = Block
+    template_name = 'blocks.html'
+    context_object_name = 'blocks'
+    ordering = ['-start']
 
 
 class ActivityListView(ListView):
     model = Activity
-    template_name = "activities.html"
-    context_object_name = "activities"
-    ordering = ["-timestamp"]
+    template_name = 'activities.html'
+    context_object_name = 'activities'
+    ordering = ['-timestamp']
