10ae8d44dde0da1f684dafc50a927e5bc208b108
Danny Muñoz <munozd17@gmail.com>
2026-04-18 09:27:52 -0400

    add block_details

--- training/urls.py
+++ training/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ActivityListView, TemplateView, IndexView, BlockListView
+from .views import *
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('activities/', ActivityListView.as_view(), name='activity-list'),
     path('blocks/', BlockListView.as_view(), name='block-list'),
+    path('blocks/<int:pk>/', BlockDetailView.as_view(), name='block-detail'),
+    path('cycles/', CycleListView.as_view(), name='cycle-list'),
 ]
--- training/views.py
+++ training/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, TemplateView
+from django.views.generic import ListView, TemplateView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Block, Cycle, Activity, Segment
 
 class IndexView(TemplateView):
@@ -8,8 +8,20 @@ class IndexView(TemplateView):
 
 class BlockListView(ListView):
     model = Block
-    template_name = 'blocks.html'
+    template_name = 'block_list.html'
     context_object_name = 'blocks'
+    ordering = ['-start']
+
+
+class BlockDetailView(DetailView):
+    model = Block
+    template_name = 'block_details.html'
+    context_object_name = 'current_block'
+
+class CycleListView(ListView):
+    model = Cycle
+    template_name = 'cycles.html'
+    context_object_name = 'cycles'
     ordering = ['-start']
 
 
