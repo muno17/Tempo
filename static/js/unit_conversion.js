00a24704d8edaf7f91e09aaab6b4d66415955424
Danny Muñoz <munozd17@gmail.com>
2026-04-27 12:31:40 -0400

    add unit_conversion.js to convert mi to km when toggled

--- static/js/unit_conversion.js
+++ static/js/unit_conversion.js
@@ -1,1 +1,50 @@
-// mi to km Miles * 1.60934
+let toggle = document.getElementById('km_toggle')
+
+toggle.addEventListener('change', (e) => {
+    if (e.target.checked) {
+        toggleKm()
+    } else {
+        toggleMi()
+    }
+})
+
+function toggleMi() {
+    let distance = document.querySelectorAll('.distance')
+    let labels = document.querySelectorAll('.distance-label')
+    let durations = document.querySelectorAll('.pace')
+
+    for (let dist of distance) {
+        dist.innerHTML = parseFloat(dist.dataset.mi)
+    }
+
+    for (let label of labels) {
+        label.innerHTML = 'mi'
+    }
+
+    for (let duration of durations) {
+        duration.innerHTML = duration.dataset.miPace
+    }
+
+}
+
+function toggleKm() {
+    let distance = document.querySelectorAll('.distance')
+    let labels = document.querySelectorAll('.distance-label')
+    let durations = document.querySelectorAll('.pace')
+
+    for (let dist of distance) {
+        dist.innerHTML = (parseFloat(dist.dataset.mi) * 1.60934).toFixed(2)
+    }
+
+    for (let label of labels) {
+        label.innerHTML = 'km'
+    }
+
+    for (let duration of durations) {
+        let distance = (parseFloat(duration.dataset.mi) * 1.60934).toFixed(2)
+        let totalSeconds = duration.dataset.seconds
+        let minutes = parseInt((totalSeconds / distance) / 60)
+        let seconds = parseInt((totalSeconds /distance) % 60)
+        duration.innerHTML = `${minutes}:${seconds}`
+    }
+}
