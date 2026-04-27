692bf6941be96e66835f44d648863dcfb8debf3c
Danny Muñoz <munozd17@gmail.com>
2026-04-27 12:40:56 -0400

    added unit_tags to add a templated version of the html for conversions

---
+++ training/templatetags/unit_tags.py
@@ -0,0 +1,20 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter
+def as_distance(value):
+    html = (f'<span class="distance" data-mi="{value}">{value}</span>'
+            f'<span class="distance-label">mi</span>')
+    return mark_safe(html)
+
+@register.simple_tag
+def pace_display(seconds, mileage, pace_str):
+    html = (f'<span class="pace"'
+            f'data-seconds="{seconds}"'
+            f'data-mi="{mileage}"'
+            f'data-mi-pace="{pace_str}">'
+            f'{pace_str}</span>'
+            f'<span class="distance-label">mi</span>')
+    return mark_safe(html)
