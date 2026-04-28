fca9332fa7d675848080068f5bfc0139395ff75d
Danny Muñoz <munozd17@gmail.com>
2026-04-27 20:42:51 -0400

    add js to make all rows on tables clickable

---
+++ static/js/row_selector.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const rows = document.querySelectorAll('tr[data-href]');
+    rows.forEach(row => {
+        row.style.cursor = 'pointer';
+        row.addEventListener('click', (e) => {
+            console.log("clicked")
+            // allow buttons to redirect to their proper pages
+            if (e.target.tagName !== 'A' && e.target.tagName !== 'BUTTON') {
+                window.location.href = row.dataset.href;
+            }
+        });
+    });
+});
