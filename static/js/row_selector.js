1b9a2212f9b761dd03386234cf2170c777c302f4
Danny Muñoz <munozd17@gmail.com>
2026-04-28 14:12:41 -0400

    adjust logo styling

--- static/js/row_selector.js
+++ static/js/row_selector.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
         row.style.cursor = 'pointer';
         row.addEventListener('click', (e) => {
             // allow buttons to redirect to their corresponding pages
+            // don't overwrite functionality for buttons
             if (e.target.tagName !== 'A' && e.target.tagName !== 'BUTTON') {
                 window.location.href = row.dataset.href;
             }
