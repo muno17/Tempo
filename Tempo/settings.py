061764cb7705a1d3b1a8541768c0815014336d4f
Danny Muñoz <munozd17@gmail.com>
2026-04-28 22:50:17 -0400

    update settings, change name of `static` folder to `assets`

--- Tempo/settings.py
+++ Tempo/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'assets/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'assets',
 ]
 
 LOGIN_REDIRECT_URL = '/'
